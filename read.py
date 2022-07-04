2e6441b160d57cf1143f8fb442cbb8b542dc6b1b
noctildon <noctildon2@gmail.com>
2022-07-04 14:15:28 -0500

    plot certain riders birthplace

--- read.py
+++ read.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from mpl_toolkits.basemap import Basemap
 from helper import *
+from ast import literal_eval
 
 
 # rider = 'tadej-pogacar'
@@ -16,10 +17,10 @@ def race(rider):
 def data_cleaning(verbose=False):
     riders_info = pd.read_csv('riders_info.csv')
 
-    # -1 = missing value, and convert to integer
-    riders_info['PCS rank'] = riders_info['PCS rank'].fillna(-1).astype(int)
-    riders_info['UCI rank'] = riders_info['UCI rank'].fillna(-1).astype(int)
-    riders_info['All time rank'] = riders_info['All time rank'].fillna(-1).astype(int)
+    # 1e6 = missing value for the rankings
+    riders_info['PCS rank'] = riders_info['PCS rank'].fillna(1e6).astype(int)
+    riders_info['UCI rank'] = riders_info['UCI rank'].fillna(1e6).astype(int)
+    riders_info['All time rank'] = riders_info['All time rank'].fillna(1e6).astype(int)
 
     # convert DOB to datetime
     riders_info['DOB'] = pd.to_datetime(riders_info['DOB'])
@@ -53,6 +54,7 @@ def hw_plot():
     plt.show()
 
 
+# Find long and lat for birth places
 def find_birth_places():
     riders_info = data_cleaning()
 
@@ -67,7 +69,7 @@ def find_birth_places():
         except:
             print(f'cannot find lon and lat for {place}\n')
             place_map_fail.append(place)
-        time.sleep(1.5)
+        time.sleep(1.5) # to avoid getting blocked by the server
 
     print('\n\nDone searching....')
     place_map_df = pd.DataFrame(place_map, columns=['address', 'lon', 'lat'])
@@ -80,16 +82,43 @@ def find_birth_places():
             f.write(f'{place}\n')
 
 
+# Create new columns (lon, lat) for birth place
+def attach_birth_place_long_lat():
+    riders_info = data_cleaning()
+
+    places = pd.read_csv('place_map.csv')
+    def finding_lon_lat(place):
+        for i in range(len(places)):
+            if place == places.iloc[i]['address']:
+                return (places.iloc[i]['lon'], places.iloc[i]['lat'])
+        return None, None
+
+    riders_info['lonlat'] = riders_info['birth place'].apply(lambda x: finding_lon_lat(x))
+    riders_info.to_csv('riders_info_cleaned.csv', index=False)
+
+
 # plot geographical birth place
-def birth_place_plot():
+# sortby = 'UCI rank', 'weight', 'dob', etc
+# numbers = how many riders to plot
+def birth_place_plot(sortby='UCI rank', numbers=100, ascending=True):
+    riders_info = pd.read_csv('riders_info_cleaned.csv')
+
     plt.figure(figsize=(8, 8))
     m = Basemap(projection='cyl', resolution=None, llcrnrlat=-90, urcrnrlat=90, llcrnrlon=-180, urcrnrlon=180,lat_0=0, lon_0=0)
     # m = Basemap(projection='moll', resolution=None, lat_0=0, lon_0=0) # for mollweide projection
     m.etopo(scale=0.5, alpha=0.5)
 
-    places = pd.read_csv('place_map.csv')
-    for x,y in zip(places['lon'].to_numpy(), places['lat'].to_numpy()):
-        m.plot(x, y, 'bo', markersize=5)
+    # sort the riders
+    riders_info = riders_info.sort_values(by=sortby, ascending=ascending).head(numbers)
+    print(f'Plotting first {numbers} riders in {sortby}')
+    print('\nPrint the first 5 riders:')
+    print(riders_info.head())
+
+    # mark the rider on the map
+    for xy in riders_info['lonlat'].to_numpy():
+        x, y = literal_eval(xy)
+        if x and y:
+            m.plot(x, y, 'bo', markersize=1)
     plt.show()
 
 
@@ -111,6 +140,7 @@ if __name__ == "__main__":
     # race(rider)
 
     # data_cleaning()
+    # attach_birth_place_long_lat()
 
     # correlation()
 
