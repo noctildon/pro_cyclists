962ff2167c77de987b851d74cf93680ee9b4b6a3
noctildon <noctildon2@gmail.com>
2022-08-15 10:19:06 -0500

    fix BMI curve fitting

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,4 +1,3 @@
-from random import gauss
 import pandas as pd
 import numpy as np
 from scipy.optimize import curve_fit
@@ -43,10 +42,6 @@ def hw_plot():
     plt.show()
 
 
-def gaussian(x, mu, sigma):
-    return 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
-
-
 def BMI_plot(save=False):
     riders_info = pd.read_csv('data/processed/riders_info_cleaned.csv')
     riders_info['BMI'] = riders_info['weight'] / riders_info['height'] ** 2
@@ -57,20 +52,23 @@ def BMI_plot(save=False):
     counts, bins, bars = plt.hist(riders_info['BMI'], bins=50)
     bin_centers = (bins[1:] + bins[:-1]) / 2
 
-    popt, pcov = curve_fit(gaussian, bin_centers, counts, bounds=((0,0), (30,15)))
-    counts_fit = gaussian(bin_centers, *popt)
+    # gaussian fit
+    def gaussian(x, mu, sigma, c):
+        return c * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
 
+    maxidx = np.argmax(counts)
+    mean = bin_centers[maxidx]
+    std = np.std(counts)
+    popt, pcov = curve_fit(gaussian, bin_centers, counts, p0=(mean, std, 1), bounds=(0, np.inf))
+    counts_fit = gaussian(bin_centers, *popt)
     print(popt)
 
-    # plt.plot(bin_centers, counts_fit, 'r-')
-    # plt.show()
-
-
-    return
-
+    plt.plot(bin_centers, counts_fit, 'r-', label=rf'$\mu={popt[0]:.2f}, \sigma={popt[1]:.2f}, c={popt[2]:.2f}$')
     plt.xlabel('BMI')
     plt.ylabel('Count')
-    plt.show()
+    plt.title('Histogram of BMI with ' + r'$f(x)=c\,\,  exp(-0.5 [ \frac{x-\mu}{\sigma}]^2)$')
+    plt.legend()
+    plt.tight_layout()
 
     if save:
         plt.savefig('reports/figures/BMI_hist.png')
@@ -100,6 +98,7 @@ def birth_place_plot(sortby='UCI rank', numbers=100, ascending=True):
         if x and y:
             m.plot(x, y, 'bo', markersize=1)
     plt.show()
+
 
 
 if __name__ == "__main__":
